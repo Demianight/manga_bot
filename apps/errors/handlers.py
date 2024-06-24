af3db385065aba1cacef0aef331f1012cf1c050c
Demianight <komarovdemian@yandex.ru>
2024-06-25 00:10:52 +0300

    feat: stupid error ignoring

--- apps/errors/handlers.py
+++ apps/errors/handlers.py
@@ -1,4 +1,5 @@
 from aiogram import F, Router
+from aiogram.exceptions import TelegramNetworkError
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ErrorEvent, Message
 from peewee import DoesNotExist
@@ -13,15 +14,17 @@ router = Router()
 async def handle_my_custom_exception(event: ErrorEvent, message: Message, state: FSMContext):
     if isinstance(event.exception, RequestException):
         return await message.answer("Кажется что-то пошло не так при ответе MangaDex, возвращаю вас в главное меню.")
-    if isinstance(event.exception, DoesNotExist):
+    elif isinstance(event.exception, DoesNotExist):
         return await message.answer(
             "Пожалуйста, нажмите /start. Если это сообщение не пропадает обратитесь к @komar197"
         )
-
-    await message.answer(
-        "Что то сломалось((. Возвращаю вас в главное меню. "
-        "Можете попробовать снова. Разработчики уже оповещены об ошибке.",
-        reply_markup=main_kb()
-    )
-    await state.clear()
-    raise event.exception
+    elif isinstance(event.exception, TelegramNetworkError):
+        raise event.exception
+    else:
+        await message.answer(
+            "Что то сломалось((. Возвращаю вас в главное меню. "
+            "Можете попробовать снова. Разработчики уже оповещены об ошибке.",
+            reply_markup=main_kb()
+        )
+        await state.clear()
+        raise event.exception
